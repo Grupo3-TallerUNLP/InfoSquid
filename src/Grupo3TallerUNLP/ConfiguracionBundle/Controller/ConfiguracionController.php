e18c7591c40318fba9cd797930db85de78d1d76c
fenusa0 <fenusa0@gmail.com>
2014-11-26 19:54:53 -0300

    Editar Configuracion

--- src/Grupo3TallerUNLP/ConfiguracionBundle/Controller/ConfiguracionController.php
+++ src/Grupo3TallerUNLP/ConfiguracionBundle/Controller/ConfiguracionController.php
@@ -29,85 +29,6 @@ class ConfiguracionController extends Controller
             'entities' => $entities,
         ));
     }
-    /**
-     * Creates a new Configuracion entity.
-     *
-     */
-    public function createAction(Request $request)
-    {
-        $entity = new Configuracion();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('configuracion_show', array('id' => $entity->getId())));
-        }
-
-        return $this->render('Grupo3TallerUNLPConfiguracionBundle:Configuracion:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
-
-    /**
-     * Creates a form to create a Configuracion entity.
-     *
-     * @param Configuracion $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Configuracion $entity)
-    {
-        $form = $this->createForm(new ConfiguracionType(), $entity, array(
-            'action' => $this->generateUrl('configuracion_create'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Crear'));
-
-        return $form;
-    }
-
-    /**
-     * Displays a form to create a new Configuracion entity.
-     *
-     */
-    public function newAction()
-    {
-        $entity = new Configuracion();
-        $form   = $this->createCreateForm($entity);
-
-        return $this->render('Grupo3TallerUNLPConfiguracionBundle:Configuracion:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
-    }
-
-    /**
-     * Finds and displays a Configuracion entity.
-     *
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('Grupo3TallerUNLPConfiguracionBundle:Configuracion')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Configuracion entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('Grupo3TallerUNLPConfiguracionBundle:Configuracion:show.html.twig', array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
 
     /**
      * Displays a form to edit an existing Configuracion entity.
@@ -124,12 +45,10 @@ class ConfiguracionController extends Controller
         }
 
         $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('Grupo3TallerUNLPConfiguracionBundle:Configuracion:edit.html.twig', array(
             'entity'      => $entity,
             'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -151,6 +70,7 @@ class ConfiguracionController extends Controller
 
         return $form;
     }
+
     /**
      * Edits an existing Configuracion entity.
      *
@@ -165,60 +85,18 @@ class ConfiguracionController extends Controller
             throw $this->createNotFoundException('Unable to find Configuracion entity.');
         }
 
-        $deleteForm = $this->createDeleteForm($id);
         $editForm = $this->createEditForm($entity);
         $editForm->handleRequest($request);
 
         if ($editForm->isValid()) {
             $em->flush();
 
-            return $this->redirect($this->generateUrl('configuracion', array('id' => $id)));
+            return $this->redirect($this->generateUrl('configuracion'));
         }
 
         return $this->render('Grupo3TallerUNLPConfiguracionBundle:Configuracion:edit.html.twig', array(
             'entity'      => $entity,
             'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
         ));
-    }
-    /**
-     * Deletes a Configuracion entity.
-     *
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('Grupo3TallerUNLPConfiguracionBundle:Configuracion')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Configuracion entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('configuracion'));
-    }
-
-    /**
-     * Creates a form to delete a Configuracion entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('configuracion_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
     }
 }
