af8ef4f62e5f8e00dce85112557821c55fcdf1be
fenusa0 <fenusa0@gmail.com>
2014-11-30 17:27:29 -0300

    Fix vista de generar informe - fix warning controlador

--- src/Grupo3TallerUNLP/InformeBundle/Controller/InformeController.php
+++ src/Grupo3TallerUNLP/InformeBundle/Controller/InformeController.php
@@ -16,17 +16,17 @@ class InformeController extends Controller
     {
         return $this->render('Grupo3TallerUNLPInformeBundle:Informe:index.html.twig');
     }
-	
+
 	public function generarAction()
 	{
-		$em = $this->getDoctrine()->getEntityManager();
+		$em = $this->getDoctrine()->getManager();
 		$user = $this->get('security.context')->getToken()->getUser();
 		$plantillas = $em->getRepository('Grupo3TallerUNLPPlantillaBundle:Plantilla')->findByUsuariosistema($user);
 		$oficinas = $em->getRepository('Grupo3TallerUNLPOficinaBundle:Oficina')->findAll();
 		$usuarios = $em->getRepository('Grupo3TallerUNLPUsuarioRedBundle:UsuarioRed')->findAll();
 		$grupos = $em->getRepository('Grupo3TallerUNLPGrupoBundle:Grupo')->findAll();
 		$sitios = $em->getRepository('Grupo3TallerUNLPSitioBundle:Sitio')->findAll();
-		
+
 		return $this->render('Grupo3TallerUNLPInformeBundle:Informe:generarInforme.html.twig',array(
 			'plantillas' => $plantillas,
 			'oficinas'	=> $oficinas,
@@ -35,31 +35,31 @@ class InformeController extends Controller
 			'sitios'	=> $sitios,
 		));
 	}
-	
+
 	public function mostrarPlantillaAction($id)
 	{
-		$em = $this->getDoctrine()->getEntityManager();
+		$em = $this->getDoctrine()->getManager();
 		$user = $this->get('security.context')->getToken()->getUser();
 		$valorfiltro = $em->getRepository('Grupo3TallerUNLPPlantillaBundle:ValorFiltro')->findByPlantilla($id);
-		
+
 		return $this->render('Grupo3TallerUNLPInformeBundle:Informe:mostrarPlantilla.html.twig',array(
 			'resultados' => $resultados,
 		));
 	}
-	
+
 	public function mostrarFiltroAction()
 	{
-		$em = $this->getDoctrine()->getEntityManager();
+		$em = $this->getDoctrine()->getManager();
 		$user = $this->get('security.context')->getToken()->getUser();
 		//consultas
-		
+
 		return $this->render('Grupo3TallerUNLPInformeBundle:Informe:mostrarFiltro.html.twig',array(
 			'resultados' => $resultados,
 		));
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 }
