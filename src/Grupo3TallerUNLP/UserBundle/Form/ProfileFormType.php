95989635503350432339bfec9004b0345219af7b
fenusa0 <fenusa0@gmail.com>
2014-12-06 21:51:48 -0300

    Correcciones

--- src/Grupo3TallerUNLP/UserBundle/Form/ProfileFormType.php
+++ src/Grupo3TallerUNLP/UserBundle/Form/ProfileFormType.php
@@ -2,20 +2,16 @@
 
 namespace Grupo3TallerUNLP\UserBundle\Form;
 
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
 use Grupo3TallerUNLP\UsuarioRedBundle\Form\ProfileType;
 
 class ProfileFormType extends BaseType
 {
-
     public function buildUserForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildUserForm($builder, $options);
         $builder->remove('username');
-
-        $builder->get('email')->setData(array('label'=>'pedo'));
 
         $builder->add('usuarioRed', new ProfileType());
     }
@@ -24,5 +20,4 @@ class ProfileFormType extends BaseType
     {
         return 'grupo3tallerunlp_profile';
     }
-
 }
